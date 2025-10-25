5efdb99681db62c281af806b8c416a464231e548
Harshi4499 <harshinijujjavarapu@gmail.com>
2025-10-25 16:39:09 +0530

    Variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#in shell scripting, variables are used to store data values
+##there are no data types in shell scripting, all variables are treated as strings by default
+###variables are created by simply assigning a value to a name
+####DRY - Don't Repeat Yourself
+####syntax to create a variable: variable_name=value
 x=20
 y=40
 echo "The value of x is: $x"
